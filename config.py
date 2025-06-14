c287acd2a428cda5451669f74a6a3ff871e25554
gabrielms-castro <gabrielms.castro@gmail.com>
2025-06-14 11:09:49 -0300

    chore: add values to be used

--- config.py
+++ config.py
@@ -1,2 +1,17 @@
 MAX_CHARS = 10_000
 WORKING_DIR = "./calculator"
+AGENT_MAX_ITERATIONS = 20
+MODEL_NAME = "gemini-2.0-flash-001"
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+Always answers the user's question or request in the same language as the user used in their prompt.
+""" 
